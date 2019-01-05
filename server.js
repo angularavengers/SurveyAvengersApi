21143788e0c66b1a608cca4fe0b799e568fbd5bb
Sourabh Sahu <sourabh.sahu@angularavengers.biz>
2019-01-06 02:33:13 +0530

    change for api

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const api = require("./server/routes/api");
 
 const port = process.env.port || 3000;
 
-const app = express();
+const app = express(); 
 
 // app.use(express.static(path.join(__dirname,"dist")));
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 app.use("/api",api);
 
