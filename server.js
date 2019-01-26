49fb86f6817a3342d853b809a1a1bc44d7ddc524
Sourabh Sahu <sourabh.sahu@angularavengers.biz>
2019-01-26 20:17:30 +0530

    changes for git

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const api = require("./server/routes/api");
 const favicon = require('serve-favicon');
 
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express(); 
 // app.use(express.static(path.join(__dirname,"dist")));
@@ -21,7 +21,6 @@ app.use("*",(req,res)=>{
     // res.sendFile(path.join(__dirname,'dist/index.html'));
 })
 
-app.listen(port,function(){
-    //alert(__dirname);
-    console.log("listeing at port 3000 now" + port);
-})
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
