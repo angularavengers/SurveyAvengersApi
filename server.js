bd362c71fa5a2bdca4fce37d4f548808dabd02a2
angularavengers <44036715+angularavengers@users.noreply.github.com>
2019-01-27 12:21:37 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const api = require("./server/routes/api");
 const favicon = require('serve-favicon');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 const app = express(); 
-// app.use(express.static(path.join(__dirname,"dist")));
+app.use(express.static(path.join(__dirname,"dist")));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json()); 
 app.use(express.static('public'));
@@ -17,8 +17,8 @@ app.use(favicon(path.join(__dirname, '/favicon.ico')));
 app.use("/api",api);
 
 app.use("*",(req,res)=>{
-    res.json("This is only demo app")
-    // res.sendFile(path.join(__dirname,'dist/index.html'));
+    // res.json("This is only demo app")
+    res.sendFile(path.join(__dirname,'dist/index.html'));
 })
 
 app.listen(PORT, () => {
