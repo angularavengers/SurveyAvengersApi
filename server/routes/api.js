abaf110342a188fac35b455caff77ed69e3b0038
sourabh sahu <Sourabh.Sahu@accolite.com>
2019-02-10 14:36:20 +0530

    changes

--- server/routes/api.js
+++ server/routes/api.js
@@ -76,18 +76,18 @@ router.delete("/deleteuser",(req,res)=>{
     .then(note => {
         if(!note) {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
+                message: "Note not found with id " + req.body.id
             });
         }
         res.send({message: "Note deleted successfully!"});
     }).catch(err => {
         if(err.kind === 'ObjectId' || err.name === 'NotFound') {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
+                message: "Note not found with id " + req.body.id
             });                
         }
         return res.status(500).send({
-            message: "Could not delete note with id " + req.params.noteId
+            message: "Could not delete note with id " + req.body.id
         });
     });
     })
